3985dd8d9d61934db9fe613c1dc6dc07b89482cb
chbouthe <chbouthe@MacBook-Air-de-chbouthe.local>
2025-05-16 11:52:05 +0200

    Last

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -18,21 +18,22 @@ void identify_ref(Base &p)
         (void)dynamic_cast<A&>(p);
         std::cout << "A" << std::endl;
         return;
-    } catch (std::bad_cast &) {}
+    } catch (std::exception &e) {std::cout << e.what() <<std::endl;}
     
     try
 	{
         (void)dynamic_cast<B&>(p);
         std::cout << "B" << std::endl;
         return;
-    } catch (std::bad_cast &) {}
+    } catch (std::exception &e) {std::cout << e.what() <<std::endl;}
+
     
     try
 	{
         (void)dynamic_cast<C&>(p);
         std::cout << "C" << std::endl;
         return;
-    } catch (std::bad_cast &) {}
+    } catch (std::exception &e) {std::cout << e.what() <<std::endl;}
 }
 
 Base * generate(void)
